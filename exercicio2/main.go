5b19fd91269cc0b65aad77075a0b5969e8afb211
maurilo-ux <maurilioshinoob@gmail.com>
2025-01-12 21:29:07 -0300

    alteração

--- exercicio2/main.go
+++ exercicio2/main.go
@@ -6,19 +6,9 @@ package main
 
 // Example 1:
 
-// Input: x = 121
+// Input: x = "anna"
 // Output: true
-// Explanation: 121 reads as 121 from left to right and from right to left.
-// Example 2:
 
-// Input: x = -121
-// Output: false
-// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-// Example 3:
-
-// Input: x = 10
-// Output: false
-// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
 import (
 	"fmt"
@@ -27,10 +17,6 @@ import (
 // len(x) - 1 dá o índice do último caractere da string.
 // O loop começa no último índice (len(x) - 1) e continua até i >= 0.
 // A cada iteração, o índice é decrementado com i--.
-
-// func isPalindrome(x int) bool {
-
-// }
 
 func palindrome(x string) (bool) {
 
